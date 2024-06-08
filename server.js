149187e9b53eb2dea2aa4a3dc1f0fecfceddd13b
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-06-08 13:11:26 +0530

    Update server.js
    get req with status code and base on the parameter

--- server.js
+++ server.js
@@ -2,20 +2,47 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+
+const friends = [{
+    id:0,
+    name:"Dhoni"
+},
+{
+    id:1,
+    name:"Virat"
+}
+]
+
 app.get('/',(req,res)=>{
 // res.send("Hello World")
-res.send({
-    id:1,
-    name:"Dhoni"
+res.send("First Page")
 })
+
+app.get('/friends',(req,res)=>{
+// res.send("Hello World")
+res.json(friends)
 })
+
+app.get('/friends/:friendId',(req,res)=>{
+    const friendId = Number(req.params.friendId)
+    const friend = friends[friendId]
+    if(friend){
+        res.json(friend)
+    }
+    else{
+        res.status(404).json({
+            error:"Friend doesnot exit"
+        })
+    }
+})
+
 app.get('/messages',(req,res)=>{
 res.send('<!DOCTYPE html><h1>Hello World</h2></html>')
 })
 app.post('/messages',(req,res)=>{
 console.log("Updating message")
 })
-    
+
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`)
 })
