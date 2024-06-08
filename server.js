ea8a7944826a3c97d67013213239338fe4c86d30
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-06-08 12:41:07 +0530

    starter

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.get('/',(req,res)=>{
+// res.send("Hello World")
+res.send({
+    id:1,
+    name:"Dhoni"
+})
+})
+app.get('/messages',(req,res)=>{
+res.send('<!DOCTYPE html><h1>Hello World</h2></html>')
+})
+app.post('/messages',(req,res)=>{
+console.log("Updating message")
+})
+    
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`)
 })
