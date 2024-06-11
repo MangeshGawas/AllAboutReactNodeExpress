3a1fe854a92a74cfeb9f8b22cb6d4c171a4538e0
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2024-06-11 10:46:05 +0530

    commit error handling and middleware

--- JobPortal-redesign/backend/app.js
+++ JobPortal-redesign/backend/app.js
@@ -2,10 +2,18 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const morgan = require('morgan')
+var cors = require('cors')
 const bodyParser = require('body-parser') 
 require("dotenv").config()
-var cors = require('cors')
+const cookieParser = require('cookie-parser')
+const errorHandler = require("./middleware/error");
 
+
+// import routes
+const authRoutes = require('./routes/authRoutes');
+
+// error middleware
+app.use(errorHandler);
 
 //port 
 const port = process.env.PORT || 9000
@@ -20,6 +28,23 @@ mongoose.connect('mongodb://127.0.0.1:27017/jobportal-api', {
     console.error('Failed to connect to MongoDB', err);
   });
 
+
+  //Middleware
+  app.use(morgan('dev'))
+  app.use(bodyParser.json({limit:"5mb"}))
+  app.use(bodyParser.urlencoded({
+    limit:"5mb",
+    extended:true
+  }))
+  app.use(cookieParser())
+  app.use(cors())
+
+
+//ROUTES MIDDLEWARE
+// app.get('/', (req, res) => {
+//     res.send("Hello from Node Js");
+// })
+app.use('/api', authRoutes);
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
